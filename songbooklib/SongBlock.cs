aff73718b53a2322547f8501d356ce5eb4d484c4
Gernot Lenkner <g.lenkner@outlook.de>
2021-01-28 16:25:24 +0100

    + new classes

---
+++ songbooklib/SongBlock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongBook
+{
+    public class SongBlock
+    {
+        public readonly List<Chord> Chords;
+        public readonly List<int> Breaks;
+        public string Text;
+
+        public SongBlock()
+        {
+            Chords = new List<Chord>();
+            Text = "";
+        }
+
+
+        public static SongBlock ParseInLine(string line)
+        {
+            return new SongBlock();
+        }
+
+        public static SongBlock ParseDoubleLine(string chords, string line)
+        {
+            return new SongBlock();
+        }
+
+    }
+}
